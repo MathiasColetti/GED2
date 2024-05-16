4b0ffa2dc415abbf4a59851f37a54db05e744895
Blokotec <lucas.souza@blokotecnologia.com.br>
2024-05-16 13:55:52 -0300

    teste de envio

--- src/main/java/com/colettialves/www/GED/repositories/PessoaReporitory.java
+++ src/main/java/com/colettialves/www/GED/repositories/PessoaReporitory.java
@@ -1,2 +1,8 @@
-package com.colettialves.www.GED.repositories;public interface PessoaReporitory {
+package com.colettialves.www.GED.repositories;
+
+import com.colettialves.www.GED.models.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface PessoaReporitory extends JpaRepository<Pessoa, Long> {
 }
